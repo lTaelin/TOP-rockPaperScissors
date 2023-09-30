84ca9f625a5037fd54272e714b6044d41e7d7bbd
Christian Uchima <christian.uchimas@gmail.com>
2023-09-30 18:07:29 -0500

    add functions for player's pick and playround

--- index.js
+++ index.js
@@ -1,23 +1,53 @@
 
-let playerPick = "";
 
-
-
-function getRandomInt(max, min) {
-    return Math.floor(Math.random() * (max - min) + min);
+function getUserChoice() {
+    let choice = prompt("Please pick from: Rock, paper or scissors");
+    const validOptions = ["rock", "paper" , "scissors"];
+    if (choice !== null){
+        choice = choice.toLowerCase();
+        if(validOptions.includes(choice)) {
+            return choice
+        } else {
+            window.alert(choice + " is not a valid option")
+        }
+    } else {
+        window.alert("please provide a choice")
+    }
 }
 
+
+function getRandomInt() {
+    return Math.floor(Math.random() * 3);
+}
 
 
 function getComputerChoice() {
-    if (getRandomInt(4, 1) === 1) {
-        return "Rock";
-    } else if (getRandomInt(4, 1) === 2) {
-        return "Paper";
-    } else if (getRandomInt(4, 1) === 3) {
-        return "Scissors";
+    if (getRandomInt() === 0) {
+        return "rock";
+    } else if (getRandomInt() === 1) {
+        return "paper";
+    } else  {
+        return "scissors";
     } 
 }
 
-let compPick = getComputerChoice()
-console.log(compPick)
+function playRound() {
+    const playerPick = getUserChoice();
+    const compPick = getComputerChoice();
+    console.log(compPick);
+
+    if (playerPick === "rock" && compPick === "scissors" ) {
+        console.log("You win");
+    } else if (playerPick === "scissors" && compPick=== "paper") {
+        console.log("Your win");
+    } else if (playerPick === "paper" && compPick === "rock") {
+        console.log("You got it!");
+    } else if (playerPick === compPick) {
+        console.log("it's a tie");
+    } else {
+        console.log("You lose.");
+    }
+        
+}
+
+playRound();
