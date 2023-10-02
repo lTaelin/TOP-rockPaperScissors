e74480bd2004933ec8e226ddfea96407ba68e890
Christian Uchima <christian.uchimas@gmail.com>
2023-10-02 13:17:16 -0500

    Added a function that updates a counter on win
    Now the counter will stop the loop if theres a winner

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+let playerScore;
+let computerScore;
 
 
+// get use's choice and makes it lowercase so it fits in the equiality comparation.
 function getUserChoice() {
     let choice = prompt("Please pick from: Rock, paper or scissors");
     const validOptions = ["rock", "paper" , "scissors"];
@@ -32,22 +35,42 @@ function getComputerChoice() {
 }
 
 function playRound() {
+    let result;
     const playerPick = getUserChoice();
     const compPick = getComputerChoice();
-    console.log(compPick);
+    
 
     if (playerPick === "rock" && compPick === "scissors" ) {
-        console.log("You win");
+        result = "You win! - Rock beats scissors!";
     } else if (playerPick === "scissors" && compPick=== "paper") {
-        console.log("Your win");
+        result = "You win! - sicissors beats paper!";
     } else if (playerPick === "paper" && compPick === "rock") {
-        console.log("You got it!");
+        result = "You win! - paper beats rock!";
     } else if (playerPick === compPick) {
-        console.log("it's a tie");
+        result = "it's a tie";
     } else {
-        console.log("You lose.");
+        result = "You lose";
     }
-        
+     
+    console.log(result);
+    return result
+    
 }
 
-playRound();
+function game(){
+    while(playerScore < 5 && computerScore < 5) {
+        const status = playRound();
+        if(status.includes("You win!")) {
+            playerScore++;
+        } else if(status.includes("You lose")) {
+            computerScore++;
+        }
+
+        console.log(`Player: ${playerScore} - PC: ${computerScore}`)
+    }
+// use a counter to store the amount of times each player wins
+//set the counter to a maximum of  5 wins
+//use a while or for loop to check them both(maybe one of each)
+// display the count up
+//show the winner.
+}
