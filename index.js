2b57d68aaecc62d207c75a430587657f8298f722
Christian Uchima <christian.uchimas@gmail.com>
2023-10-02 14:05:39 -0500

    Value for global var and victory msg
    Game can now start with the var given 0 and randomint secured

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let playerScore;
-let computerScore;
+let playerScore = 0;
+let computerScore = 0;
 
 
 // get use's choice and makes it lowercase so it fits in the equiality comparation.
@@ -9,12 +9,12 @@ function getUserChoice() {
     if (choice !== null){
         choice = choice.toLowerCase();
         if(validOptions.includes(choice)) {
-            return choice
+            return choice;
         } else {
-            window.alert(choice + " is not a valid option")
+            window.alert(choice + " is not a valid option");
         }
     } else {
-        window.alert("please provide a choice")
+        window.alert("please provide a choice");
     }
 }
 
@@ -25,9 +25,10 @@ function getRandomInt() {
 
 
 function getComputerChoice() {
-    if (getRandomInt() === 0) {
+    let randomNumber = getRandomInt();
+    if (randomNumber === 0) {
         return "rock";
-    } else if (getRandomInt() === 1) {
+    } else if (randomNumber === 1) {
         return "paper";
     } else  {
         return "scissors";
@@ -53,7 +54,7 @@ function playRound() {
     }
      
     console.log(result);
-    return result
+    return result;
     
 }
 
@@ -68,9 +69,17 @@ function game(){
 
         console.log(`Player: ${playerScore} - PC: ${computerScore}`)
     }
+
+    if(playerScore === 5) {
+        console.log("The player gets the win!");
+    } else if (computerScore === 5) {
+        console.log("Seems you have been defeated.");
+    }
 // use a counter to store the amount of times each player wins
 //set the counter to a maximum of  5 wins
 //use a while or for loop to check them both(maybe one of each)
 // display the count up
 //show the winner.
 }
+
+game();
